06deb537eb9b2f505871b7dd0c0793ee0bb59e26
vbelugin <belugin.v@gmail.com>
2018-03-19 17:28:35 +0200

    Financing Plan on Planet XY140Z-n

---
+++ src/test/java/FinanceTest.java
@@ -0,0 +1,25 @@
+import kyu6.Finance;
+import org.junit.Test;
+
+import java.math.BigInteger;
+
+import static org.junit.Assert.assertEquals;
+
+public class FinanceTest {
+    @Test
+    public void test1() {
+        assertEquals(BigInteger.valueOf(105), Finance.finance(5));
+    }
+    @Test
+    public void test2() {
+        assertEquals(BigInteger.valueOf(168), Finance.finance(6));
+    }
+    @Test
+    public void test3() {
+        assertEquals(BigInteger.valueOf(360), Finance.finance(8));
+    }
+    @Test
+    public void test4() {
+        assertEquals(BigInteger.valueOf(2040), Finance.finance(15));
+    }
+}
