7eb8c87c57aeaa02708822afc6876ec7726719b5
vbelugin <belugin.v@gmail.com>
2018-03-30 12:15:40 +0300

    isCryptedSolution

--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -220,4 +220,19 @@ public class SolutionTest {
         assertFalse(ArraysPractice.sudoku2(grid));
     }
 
+    @Test
+    public void isCryptSolution1() {
+        String[] crypt = new String[]{"SEND", "MORE", "MONEY"};
+        char[][] solution = new char[][] {
+                new char[]{'O', '0'},
+                new char[]{'M', '1'},
+                new char[]{'Y', '2'},
+                new char[]{'E', '5'},
+                new char[]{'N', '6'},
+                new char[]{'D', '7'},
+                new char[]{'R', '8'},
+                new char[]{'S', '9'}};
+        assertTrue(ArraysPractice.isCryptSolution(crypt, solution));
+    }
+
 }
